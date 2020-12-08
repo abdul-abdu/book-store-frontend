e5e3c8d0f78b9b1e77d38d8fb15407a77ef7701a
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-12-08 09:43:03 +0500

    Implemented modal for comments

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,35 +1,35 @@
 import { Card, Button } from 'react-bootstrap'
 import React from 'react'
+import ModalView from './Modal'
 
-class SingleBook extends React.Component {
-  sendData = (book) => {
-    this.props.grandCallback(book)
-  }
-  // constructor(pro)
-  render() {
-    return (
+const SingleBook = (props) => {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
       <Card style={{ width: '13rem' }}>
         <div className='card-img-box'>
-          <Card.Img variant="top" src={this.props.book.img} />
+          <Card.Img variant="top" src={props.book.img} />
         </div>
         <Card.Body>
           <div className='card-tex-box'>
-            <Card.Title>{this.props.book.title}</Card.Title>
-            <Card.Text>$ {this.props.book.price}</Card.Text>
+            <Card.Title>{props.book.title}</Card.Title>
+            <Card.Text>$ {props.book.price}</Card.Text>
           </div>
           <Button className='my-1' variant="outline-warning">Buy Now</Button>
           <Button
             variant="outline-info"
-            onClick={() => {
-              this.props.onClick()
-              this.sendData(this.props.book)
-            }}
+            onClick={() => setModalShow(true)}
           >Comment
         </Button>
         </Card.Body>
       </Card >
-    )
-  }
+      <ModalView
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  )
 }
 
 
