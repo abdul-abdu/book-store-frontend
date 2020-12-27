24d7df13507e74f8e50a7c86273a04c586f61363
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2020-12-27 16:54:41 +0500

    Fixed css bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,71 +24,66 @@ const BookCategories = [
 const NavBar = (props) => {
   const { updateCategory, currentCategory } = props
   return (
-    <>
-      <Navbar fixed="top" variant="dark" expand="sm">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            BookStore
-          </Navbar.Brand>
-          <Nav.Link as={Link} to="/books">
-            Booklist
-          </Nav.Link>
-          <Nav.Link href="#home">
-            <div style={{ position: "relative" }}>
-              <FiShoppingCart size={32} />
-              <Badge
-                variant="danger"
-                style={{ position: "absolute", top: "15px" }}
-              >
-                0
-              </Badge>
-            </div>
-          </Nav.Link>
+    <Navbar fixed="top" variant="dark" expand="sm">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          BookStore
+        </Navbar.Brand>
+        <Nav.Link as={Link} to="/books">
+          Booklist
+        </Nav.Link>
+        <Nav.Link href="#home">
+          <div style={{ position: "relative" }}>
+            <FiShoppingCart size={32} />
+            <Badge
+              variant="danger"
+              style={{ position: "absolute", top: "15px" }}
+            >
+              0
+            </Badge>
+          </div>
+        </Nav.Link>
+        <>
+          {!props.location.pathname.includes("details") && (
+            <Col className="pl-0">
+              <FormControl
+                style={{ minWidth: "110px" }}
+                type="text"
+                placeholder="Search"
+                className="ml-md-2 "
+                // onChange={(e) => this.props.handleSearchQuery(e.target.value)}
+              />
+            </Col>
+          )}
           <>
             {!props.location.pathname.includes("details") && (
-              <Col className="pl-0">
-                <FormControl
-                  style={{ minWidth: "110px" }}
-                  type="text"
-                  placeholder="Search"
-                  className="ml-md-2 "
-                  // onChange={(e) => this.props.handleSearchQuery(e.target.value)}
-                />
-              </Col>
+              <DropdownButton
+                menuAlign="right"
+                variant="outline-secondary"
+                // className="ml-md-2"
+                id="dropdown-basic-button"
+                title={currentCategory.toUpperCase()}
+              >
+                {BookCategories.map((category, idx) => {
+                  return (
+                    <Dropdown.Item
+                      key={idx}
+                      onClick={() => {
+                        if (category !== currentCategory) {
+                          updateCategory(category)
+                        }
+                      }}
+                    >
+                      {category.toUpperCase()}
+                    </Dropdown.Item>
+                  )
+                })}
+              </DropdownButton>
             )}
-            <>
-              {!props.location.pathname.includes("details") && (
-                <DropdownButton
-                  menuAlign="right"
-                  variant="outline-secondary"
-                  // className="ml-md-2"
-                  id="dropdown-basic-button"
-                  title={currentCategory.toUpperCase()}
-                >
-                  {BookCategories.map((category, idx) => {
-                    return (
-                      <Dropdown.Item
-                        key={idx}
-                        onClick={() => {
-                          if (category !== currentCategory) {
-                            updateCategory(category)
-                          }
-                        }}
-                      >
-                        {category.toUpperCase()}
-                      </Dropdown.Item>
-                    )
-                  })}
-                </DropdownButton>
-              )}
-            </>
           </>
-        </Container>
-      </Navbar>
-      <br />
-      <br />
-      <br />
-    </>
+        </>
+      </Container>
+    </Navbar>
   )
 }
 
