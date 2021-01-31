5cddf9d4daaef8bd653d07f2c7bfab96ae9a32ec
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-01-31 15:46:07 +0500

    Updated styles

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer className="page-footer font-small blue pt-4 bg-dark text-white">
+    <footer className="page-footer font-small blue pt-4 bg-dark text-white mt-5">
       <Container fluid>
         <Row>
           <Col>
             <h5>Footer content</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique quas animi, eos sint vitae voluptatibus consectetur quae mollitia adipisci laboriosam pariatur, explicabo quia ullam sequi modi dolor ad. Non, voluptates..</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Similique quas animi, eos sint vitae voluptatibus consectetur quae
+              mollitia adipisci laboriosam pariatur, explicabo quia ullam sequi
+              modi dolor ad. Non, voluptates..
+            </p>
           </Col>
           <hr className="clearfix w-100 d-md-none pb-3" />
           <Col>
             {/* Links */}
-            <h5 className='text-uppercase'>Links</h5>
+            <h5 className="text-uppercase">Links</h5>
             <ul>
               <li>
                 <a href="#!">Link 1</a>
@@ -29,7 +32,7 @@ const Footer = () => {
             </ul>
           </Col>
           <Col>
-            <h5 className='text-uppercase'>Links</h5>
+            <h5 className="text-uppercase">Links</h5>
             <ul>
               <li>
                 <a href="#!">Link 1</a>
@@ -45,9 +48,7 @@ const Footer = () => {
         </Row>
       </Container>
     </footer>
-
-  )
-}
-
+  );
+};
 
 export default Footer;
